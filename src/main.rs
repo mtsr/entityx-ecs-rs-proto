be0530d47e25330b08d023cf7f2a92b245d38cdc
Jonas Matser <jonas@jonasmatser.nl>
2014-12-13 17:12:27 +0100

    Test Control functionality

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use std::fmt::Show;
 use std::iter::{ IteratorExt };
 
 use ecs::{
-    // Entity,
+    Entity,
+    Control,
     EntityManager,
     System,
     SystemManager,
@@ -28,11 +29,12 @@ fn main() {
 
         let test_entity1 = entity_manager.create_entity();
         entity_manager.assign_component(&test_entity1, Renderable);
-        entity_manager.assign_component(&test_entity1, Loud);
+        entity_manager.assign_component(&test_entity1, Loud(1));
         let test_entity2 = entity_manager.create_entity();
+        entity_manager.assign_component(&test_entity2, Loud(2));
         entity_manager.assign_component(&test_entity2, Renderable);
         let test_entity3 = entity_manager.create_entity();
-        entity_manager.assign_component(&test_entity3, Loud);
+        entity_manager.assign_component(&test_entity3, Loud(3));
 
         entity_manager.destroy_entity(test_entity1);
 
@@ -41,14 +43,16 @@ fn main() {
         // entity_manager.assign_component(&test_entity4, Loud);
     }
 
-    system_manager.update::<UpdateArgs, TestSystem>(rc_entity_manager.clone(), &UpdateArgs);
+    for count in range::<uint>(1, 10) {
+        system_manager.update::<UpdateArgs, TestSystem>(&rc_entity_manager, &UpdateArgs);
+    }
 }
 
 #[deriving(Show)]
 struct Renderable;
 
 #[deriving(Show)]
-struct Loud;
+struct Loud(int);
 
 #[deriving(Show)]
 struct Player;
@@ -65,13 +69,29 @@ impl TestSystem {
 struct UpdateArgs;
 
 impl System for TestSystem {
-    fn update<A>(&self, entity_manager: Rc<RefCell<EntityManager>>, args: &A) where A: Show {
-    fn update<A>(&mut self, entity_manager: Rc<RefCell<EntityManager>>, args: &A) where A: Show {
+    fn update<A>(&mut self, entity_manager: &Rc<RefCell<EntityManager>>, control: &mut Control, args: &A) where A: Show {
         println!("1 {}", args);
         let entity_manager = entity_manager.borrow();
 
-        for (entity, renderable, option_loud) in entities_with_components!(entity_manager: with Renderable option Loud without Player) {
-            println!("{}, {}, {}", entity.id(), renderable, option_loud);
+        control.build(box |entity_manager: &mut EntityManager, entity: Entity| {
+            entity_manager.assign_component(&entity, Renderable);
+        });
+
+        for (entity, renderable, option_loud, option_player) in entities_with_components!(entity_manager: with Renderable option Loud option Player) {
+            println!("{}, {}, {}, {}", entity.id(), renderable, option_loud, option_player);
+
+            control.modify(entity, box |entity_manager: &mut EntityManager, entity: Entity| {
+                entity_manager.assign_component(&entity, Player);
+                if let &Some(ref mut loud) = entity_manager.get_component_mut::<Loud>(&entity) {
+                    loud.0 = 10;
+                };
+            });
+        }
+
+        for (entity, renderable, option_loud, option_player) in entities_with_components!(entity_manager: with Renderable option Loud option Player) {
+            if option_player.is_some() {
+                control.destroy(entity)
+            };
         }
     }
 }
