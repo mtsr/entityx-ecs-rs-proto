69bff1e118c4e0ca0ce3933fb395eff22a6d13c2
Jonas Matser <jonas@jonasmatser.nl>
2014-12-25 19:57:32 +0100

    Added system with captured get_component_data and with component_masks

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@
 
 use std::rand;
 use std::fmt::Show;
-use std::iter::{ IteratorExt };
+use std::iter::{ Iterator, IteratorExt, FilterMap };
 use std::collections::{
     HashMap,
     VecMap,
+    Bitv
 };
 
 use ecs::{
@@ -28,9 +29,9 @@ fn main() {
     entity_manager.register_component::<Cmp2>(box VecMap::new());
     entity_manager.register_component::<Cmp3>(box VecMap::new());
     entity_manager.register_component::<Cmp4>(box VecMap::new());
-    entity_manager.register_component::<Cmp5>(box HashMap::new());
+    entity_manager.register_component::<Cmp5>(box VecMap::new());
 
-    for _ in range(0u, 10000u) {
+    for _ in range(0u, 2000000u) {
         let entity = entity_manager.create_entity();
         if rand::random::<f32>() > 0.5f32 {
             entity_manager.assign_component(&entity, Cmp1);
@@ -49,20 +50,8 @@ fn main() {
         }
     }
 
-    for _ in range(0u, 1000u) {
+    for _ in range(0u, 10u) {
         system_manager.update::<uint, Sys>(&mut entity_manager, &0u);
-    }
-    // }
-
-    let component_data = entity_manager.get_component_data::<Cmp1>();
-    for (entity, component) in entity_manager.entities().filter_map(|entity: Entity<World1>| {
-        if let Some(component) = component_data.list.get(&entity.index()) {
-            return Some((entity, component))
-        } else {
-            None::<(Entity<World1>, &Cmp1)>
-        }
-    }) {
-        println!("{}, {}", entity, component);
     }
 }
 
@@ -90,7 +79,62 @@ impl<Id> System<Id, Sys> for Sys {
 
         let mut counter = 0u;
 
-        for (_, _, _, _, _) in entities_with_components!(entity_manager: without Cmp1 with Cmp2 with Cmp3 with Cmp4 with Cmp5) {
+        // for (_, _, _, _, _) in entities_with_components!(entity_manager: without Cmp1 with Cmp2 with Cmp3 with Cmp4 with Cmp5) {
+        //     counter += 1;
+        // }
+
+        let component_data = (entity_manager.get_component_data::<Cmp1>(),)
+        .tup_append(entity_manager.get_component_data::<Cmp2>())
+        .tup_append(entity_manager.get_component_data::<Cmp3>())
+        .tup_append(entity_manager.get_component_data::<Cmp4>())
+        .tup_append(entity_manager.get_component_data::<Cmp5>());
+        let mut with_mask = Bitv::from_elem(entity_manager.get_components_length(), false);
+        let mut without_mask = Bitv::from_elem(entity_manager.get_components_length(), false);
+        for tuple in entity_manager.entities().filter(|entity| {
+            with_mask.set(component_data.1.index, true);
+            with_mask.set(component_data.2.index, true);
+            with_mask.set(component_data.3.index, true);
+            with_mask.set(component_data.4.index, true);
+
+            without_mask.set(component_data.0.index, true);
+
+            let component_mask = entity_manager.get_entity_component_mask(entity);
+
+            if with_mask.intersect(component_mask) || without_mask.difference(component_mask) {
+                false
+            } else {
+                true
+            }
+        })
+        .filter_map(|entity: Entity<Id>| {
+            if let Some(component) = component_data.1.list.get(&entity.index()) {
+                return Some((entity, component));
+            } else {
+                None
+            }
+        })
+        .filter_map(|tuple| {
+            if let Some(component) = component_data.2.list.get(&tuple.0.index()) {
+                return Some(tuple.tup_append(component));
+            } else {
+                None
+            }
+        })
+        .filter_map(|tuple| {
+            if let Some(component) = component_data.3.list.get(&tuple.0.index()) {
+                return Some(tuple.tup_append(component));
+            } else {
+                None
+            }
+        })
+        .filter_map(|tuple| {
+            if let Some(component) = component_data.4.list.get(&tuple.0.index()) {
+                return Some(tuple.tup_append(component));
+            } else {
+                None
+            }
+        }) {
+            // println!("{}", tuple);
             counter += 1;
         }
     }
