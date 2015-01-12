0c26cd2794196de46f79e6aba4af355342d4c4ee
Jonas Matser <jonas@jonasmatser.nl>
2015-01-12 11:20:40 +0100

    Add window + events

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate time;
 
+extern crate glutin;
+
 extern crate ecs;
+
+use glutin::{ Window, PollEventsIterator };
 
 use ecs::{
     World,
@@ -13,49 +17,60 @@ use ecs::{
 };
 
 fn main() {
-    let mut events = Loop::new();
+    let window = glutin::Window::new().unwrap();
 
-    let mut count = 0us;
-    while let Some(event) = events.next() {
-        println!("{:?}", event);
-        count += 1;
-        if count >= 100us {
-            break;
+    unsafe { window.make_current() };
+
+    let mut events = Loop::new(&window);
+
+    loop {
+        match events.next() {
+            Some(Event::Update(dt, events)) => {
+            },
+            Some(Event::Render(ref window, dt)) => {
+            },
+            None => {
+                break;
+            }
         }
-
-        std::io::timer::sleep(std::time::duration::Duration::milliseconds(10));
     }
 }
 
-#[derive(Show)]
-enum Event {
-    Update(u64),
-    Render(u64),
+enum Event<'a> {
+    Update(u64, PollEventsIterator<'a>),
+    Render(&'a Window, u64),
 }
 
 const FIXED_TIMESTEP_NS: u64 = 1_000_000_000 / 60;
 const MAX_LOOPS: u8 = 10;
 
-struct Loop {
+struct Loop<'a> {
     loops: u8,
     accumulated_ns: u64,
     last_ns: u64,
+
+    window: &'a Window,
 }
 
-impl Loop {
-    fn new() -> Loop {
+impl<'a> Loop<'a> {
+    fn new(window: &'a Window) -> Loop<'a> {
         Loop {
             loops: 0,
             accumulated_ns: 0,
             last_ns: time::precise_time_ns(),
+            window: window,
         }
     }
 }
 
-impl Iterator for Loop {
-    type Item = Event;
+impl<'a> Iterator for Loop<'a> {
+    type Item = Event<'a>;
 
-    fn next(&mut self) -> Option<Event> {
+    fn next(&mut self) -> Option<Event<'a>> {
+        if self.window.is_closed() {
+            return None;
+        }
+
         if self.loops >= MAX_LOOPS || self.accumulated_ns < FIXED_TIMESTEP_NS {
             let current_ns = time::precise_time_ns();
             let delta_ns = current_ns - self.last_ns;
@@ -65,12 +80,19 @@ impl Iterator for Loop {
 
             self.loops = 0;
 
-            return Some(Event::Render(delta_ns));
+            return Some(Event::Render(self.window, delta_ns));
         }
+
+        // this is after a render
+        if self.loops == 0 {
+            self.window.swap_buffers();
+        }
+
+        let events = self.window.poll_events();
 
         self.accumulated_ns -= FIXED_TIMESTEP_NS;
         self.loops += 1;
 
-        Some(Event::Update(FIXED_TIMESTEP_NS))
+        Some(Event::Update(FIXED_TIMESTEP_NS, events))
     }
 }
